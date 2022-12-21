e535333ba12cda6e9c6c81c843af13ac74f7bf00
Andre <andre.soares@virtual-identity.com>
2022-12-21 10:26:05 +0000

    fix: agency language api

--- src/api/apiAgencyLanguages.ts
+++ src/api/apiAgencyLanguages.ts
@@ -1,5 +1,5 @@
 import { endpoints } from '../resources/scripts/endpoints';
-import { fetchData, FETCH_ERRORS, FETCH_METHODS } from './fetchData';
+import { fetchData, FETCH_SUCCESS, FETCH_METHODS } from './fetchData';
 
 import { AgencyLanguagesInterface } from '../globalState';
 
@@ -13,7 +13,7 @@ export const apiAgencyLanguages = async (
 		url: url,
 		method: FETCH_METHODS.GET,
 		skipAuth: true,
-		responseHandling: [FETCH_ERRORS.EMPTY],
+		responseHandling: [FETCH_SUCCESS.CONTENT],
 		...(useMultiTenancyWithSingleDomain && {
 			headersData: { agencyId }
 		})
